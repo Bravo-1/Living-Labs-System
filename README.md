196119e05617e0d012d84b668c06d656f5859f2c
MAKOSCAFEE <barnabasmakonda@gmail.com>
2015-07-21 14:18:06 +0300

    changed request alot of files

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-LL living lab network project is an online visual tool that presents the evolution of
-the different Tanzanian LLs as well as their goals, objectives, challenges and
-programmes. It allows its members to focus on their objectives and gather ideas
-from others. It is also a tool for funders to evaluate the effectiveness of the LLs in
-using funds and attain sustainability.
+heroku-node
+===========
+
+Code for the tutorial by @sevilayha: Deploying Node Apps to Heroku
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swig = require('swig');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -16,8 +13,7 @@ app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -25,38 +21,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// set the home page route
+app.get('/', function(req, res) {
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	// ejs render automatically looks in the views folder
+	res.render('index');
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 
 app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+	console.log('Our app is running on http://localhost:' + app.get('port'));
 });
